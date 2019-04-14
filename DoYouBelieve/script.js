44f69c5fdba181c5ec3d2ccfa85ad1d0f82bbb9e
jamiefule <38890996+jamiefule@users.noreply.github.com>
2019-04-13 21:30:55 -0500

    Add files via upload

--- DoYouBelieve/script.js
+++ DoYouBelieve/script.js
@@ -1,3 +1,4 @@
+var runes = ["B E L I E V E", "MAGIC IS REAL", "MAGIC E X I S T S", "THIS IS SAFE", "DO NOT BE AFRAID", "MAGIC HEALS", "LOVE THE EARTH", "BE CAREFUL", "DO NOT WORRY"];
 $(document).ready(function() {
   $("#believe").on("change", function(){
     if($("#believe").val() == "yes"){
@@ -5,8 +6,12 @@ $(document).ready(function() {
       $("#window").css("backgroundImage", "url('assets/yes-window.png')");
     }
     else{
-      $("#content").css("backgroundImage", "url('assets/no-room-empty.png')");
+      $("#content").css("backgroundImage", "url('assets/no-room-full.png')");
       $("#window").css("backgroundImage", "url('assets/no-window.png')");
     }
   });
+  setInterval(function(){
+    $("#runes-left").text(runes[Math.floor(Math.random() * runes.length)]);
+    $("#runes-right").text(runes[Math.floor(Math.random() * runes.length)]);
+}, 300);
 });
