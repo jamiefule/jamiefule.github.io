8ba1777d03250bf94fc9fe6ec83aad1bcd4708d5
jamiefule <38890996+jamiefule@users.noreply.github.com>
2019-04-13 21:38:03 -0500

    Add files via upload

--- DoYouBelieve/script.js
+++ DoYouBelieve/script.js
@@ -1,17 +1,31 @@
 var runes = ["B E L I E V E", "MAGIC IS REAL", "MAGIC E X I S T S", "THIS IS SAFE", "DO NOT BE AFRAID", "MAGIC HEALS", "LOVE THE EARTH", "BE CAREFUL", "DO NOT WORRY"];
+var believe = false;
 $(document).ready(function() {
-  $("#believe").on("change", function(){
+  $("#believe").on("click", function(){
     if($("#believe").val() == "yes"){
+      $("#content-border").css("backgroundColor","white");
+      believe = true;
       $("#content").css("backgroundImage", "url('assets/yes-room-empty.png')");
       $("#window").css("backgroundImage", "url('assets/yes-window.png')");
     }
     else{
+      $("#content-border").css("backgroundColor","white");
+      believe = false;
       $("#content").css("backgroundImage", "url('assets/no-room-full.png')");
       $("#window").css("backgroundImage", "url('assets/no-window.png')");
     }
   });
+
   setInterval(function(){
-    $("#runes-left").text(runes[Math.floor(Math.random() * runes.length)]);
-    $("#runes-right").text(runes[Math.floor(Math.random() * runes.length)]);
+      if(believe == true){
+        $("#runes-left").css("visibility","visible");
+        $("#runes-right").css("visibility","visible");
+        $("#runes-left").text(runes[Math.floor(Math.random() * runes.length)]);
+        $("#runes-right").text(runes[Math.floor(Math.random() * runes.length)]);
+      } else{
+      $("#runes-left").css("visibility","hidden");
+      $("#runes-right").css("visibility","hidden");
+
+      }
 }, 300);
 });
