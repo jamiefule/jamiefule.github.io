429f391f8b6866bb313a5631d7c78ddb4bdcee0b
Jamie Fule <jamiefule@gmail.com>
2021-05-22 21:26:30 -0700

    tyling changes plus tempterature addition

--- professional-content/JS/scripts/setup.js
+++ professional-content/JS/scripts/setup.js
@@ -70,26 +70,14 @@ function populateWeeklyForcast(){
                 data = JSON.parse(data)
                 console.log(data);
                 for(var i = 0; i < 7; i++){
-                    $("#day-" + (i+1)).append("<p class='weather-report'>" + weatherCleanup(data.dataseries[i].weather)+ "</p>")
+                    $("#day-" + (i+1)).append("<p class='weather-report'>" + weatherCleanup("High: " + (parseInt(data.dataseries[i].temp2m["max"])* 9/5 + 32) + " | Low:" + (parseInt(data.dataseries[i].temp2m["min"])* 9/5 + 32))+ "</p>")
                     $("#day-" + (i+1)).append("<i class='weather-icon fa-2x " + weatherKey[weatherCleanup(data.dataseries[i].weather)] +"'></i>")
             
                 }
-                //fill out wind
-                var windVal = data.dataseries[0].wind10m_max
-                var windMapping = {
-                    1: "Below 0.3m/s (Calm)",
-                    2: "0.3-3.4m/s (Light)",
-                    3: "3.4-8.0m/s (Moderate)",
-                    4: "8.0-10.8m/s (Fresh)",
-                    5: "10.8-17.2m/s (Strong)",
-                    6: "17.2-24.5m/s (Gale)",
-                    7: "24.5-32.6m/s (Storm)",
-                    8: "Over 32.6m/s (Hurricane)"
-                }
-                $("#wind-card").append("<p id='wind-val'>" + windMapping[windVal] + "</p>")
 
-                //remove loading gif
-                $(".loading-row td img").css("display", "none")
+                //remove loading gif and blur filter
+                $("#spinner-container img").css("display", "none")
+                $("#fuzzy").css("display","none")
         }   
     });
 
